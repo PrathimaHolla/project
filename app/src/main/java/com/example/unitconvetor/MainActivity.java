e0465d35e460b5db6b68ec57cf37d7bb58b2bb34
PrathimaHolla <prathimaholla1606@gmail.com>
2021-06-11 17:54:19 +0530

    part1 ui for temperature

---
+++ app/src/main/java/com/example/unitconvetor/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.unitconvetor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void changeUnit(View view){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
