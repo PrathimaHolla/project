d81e0597efee1c2d7db884dd1d725684638bd670
PrathimaHolla <prathimaholla1606@gmail.com>
2021-06-11 22:25:12 +0530

    temp changes

--- app/src/main/java/com/example/unitconvetor/MainActivity.java
+++ app/src/main/java/com/example/unitconvetor/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    String[] mobileArray = {"Celsius","Kelvin","Fahrenheit"};
 
     public void changeUnit(View view){
 
@@ -15,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
     }
 }
